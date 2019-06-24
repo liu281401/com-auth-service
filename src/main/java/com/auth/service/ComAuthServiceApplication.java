147d0d7db875e45570e70dda049278aaf5d7a434
liuyu <12345678>
2019-06-24 17:16:47 +0800

    Initial commit

---
+++ src/main/java/com/auth/service/ComAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComAuthServiceApplication.class, args);
+    }
+
+}
